8511cafe665ee1e9e69e7c184ae91f2e55e2ec4f
Nikita Kazakov <contactnikitakazakov@gmail.com>
2019-04-04 15:56:07 -0400

    MVC with flix

--- flix/app/controllers/movies_controller.rb
+++ flix/app/controllers/movies_controller.rb
@@ -1,7 +1,8 @@
 class MoviesController < ApplicationController
 
   def index
-    @movies = ["Iron Man", "Superman", "Spider-Man", "Die Hard"]
+    #@movies = ["Iron Man", "Superman", "Spider-Man", "Die Hard"] Hardcoded...
+    @movies = Movie.all
   end
 
 end
