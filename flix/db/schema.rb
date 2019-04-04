56af7f104ead9de9b56c3d1048c804b6196fa606
Nikita Kazakov <contactnikitakazakov@gmail.com>
2019-04-04 15:03:35 -0400

    movies model created.

--- flix/db/schema.rb
+++ flix/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190403012953) do
+ActiveRecord::Schema.define(version: 20190404183525) do
 
   create_table "events", force: :cascade do |t|
     t.string   "name"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 20190403012953) do
     t.decimal  "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "movies", force: :cascade do |t|
+    t.string   "title"
+    t.string   "rating"
+    t.decimal  "total_gross"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
