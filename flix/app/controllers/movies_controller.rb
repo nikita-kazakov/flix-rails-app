0493a3991e7d71f42fa4d0fc7e98c40bed2508f2
Nikita Kazakov <contactnikitakazakov@gmail.com>
2019-05-05 22:28:18 -0400

    Editing Forms

--- flix/app/controllers/movies_controller.rb
+++ flix/app/controllers/movies_controller.rb
@@ -13,4 +13,11 @@ class MoviesController < ApplicationController
     @movie = Movie.find(params[:id])
   end
 
+  def update
+    @movie = Movie.find(params[:id])
+    movie_params = params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross)
+    @movie.update(movie_params)
+    redirect_to @movie
+  end
+
 end
--- flix/config/routes.rb
+++ flix/config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'movies' => 'movies#index'
   get 'movies/:id' =>'movies#show', as:'movie'
   get 'movies/:id/edit' => 'movies#edit', as: "edit_movie"
+  patch 'movies/:id' => 'movies#update'
 
   #Below are rails routes I do for practice
   get 'flashdrives' => 'flashdrives#index'
