5aedfcdbb444ca3ff8b1e2f6dd49d7361e4f4536
Nikita Kazakov <contactnikitakazakov@gmail.com>
2019-05-07 15:14:46 -0400

    Adding custom queries to movies.

--- flix/app/controllers/movies_controller.rb
+++ flix/app/controllers/movies_controller.rb
@@ -2,7 +2,8 @@ class MoviesController < ApplicationController
 
   def index
     #@movies = ["Iron Man", "Superman", "Spider-Man", "Die Hard"] Hardcoded...
-    @movies = Movie.all
+    #@movies = Movie.all
+    @movies = Movie.released #<==This is defined in the Movie Model
   end
 
   def show
--- flix/app/models/movie.rb
+++ flix/app/models/movie.rb
@@ -4,4 +4,13 @@ class Movie < ApplicationRecord
     self.total_gross.blank? || self.total_gross < 50000000
   end
 
+  def self.released
+    where("released_on <= ?", Time.now).order("released_on desc")
+  end
+
+  #This method lists most recently added movies.
+  def self.recently_added
+    order('created_at desc').limit(3)
+  end
+
 end
