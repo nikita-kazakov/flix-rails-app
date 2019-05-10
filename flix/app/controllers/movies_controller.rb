28b0831f03bc5c8e10339f3399143293fb9c8419
Nikita Kazakov <contactnikitakazakov@gmail.com>
2019-05-10 14:07:22 -0400

    Movies with Validation messages

--- flix/app/controllers/movies_controller.rb
+++ flix/app/controllers/movies_controller.rb
@@ -14,22 +14,30 @@ class MoviesController < ApplicationController
     @movie = Movie.find(params[:id])
   end
 
-  def update
-    @movie = Movie.find(params[:id])
-    #movie_params = params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross)
-    @movie.update(movie_params)
-    redirect_to @movie
-  end
+
 
   def new
     @movie = Movie.new
   end
 
+  def update
+    @movie = Movie.find(params[:id])
+    if @movie.update(movie_params)
+      redirect_to @movie
+    else
+      render :edit
+    end
+
+  end
+
   def create
     #movie_params = params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross)
     @movie = Movie.create(movie_params)
-    @movie.save
-    redirect_to movies_path
+    if @movie.save
+      redirect_to movies_path
+    else
+      render :new
+    end
   end
 
   def destroy
