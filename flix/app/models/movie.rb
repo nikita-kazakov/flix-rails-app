3fa363cecac1b428b08e8efe06aea3d624654a80
Nikita Kazakov <contactnikitakazakov@gmail.com>
2019-05-09 23:25:47 -0400

    flix validations.

--- flix/app/models/movie.rb
+++ flix/app/models/movie.rb
@@ -1,5 +1,17 @@
 class Movie < ApplicationRecord
 
+  validates :title, :released_on, :duration, presence: true
+  validates :description, length:{minimum: 25}
+  validates :total_gross, numericality: {greater_than_or_equal_to:0}
+  validates :image_file_name, allow_blank: true, format: {
+      with:    /\w+\.(gif|jpg|png)\z/i,
+      message: "must reference a GIF, JPG, or PNG image"
+  }
+
+  RATINGS = ["G", "PG", "PG-13", "R", "NC-17"]
+  validates :rating, inclusion: { in:RATINGS }
+
+
   def flop?
     self.total_gross.blank? || self.total_gross < 50000000
   end
