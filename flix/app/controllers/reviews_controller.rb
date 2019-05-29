6c8448e7803efa9e49332ba49f956c8fa074a4fe
Nikita Kazakov <contactnikitakazakov@gmail.com>
2019-05-29 15:10:33 -0400

    Added Review Form.

--- flix/app/controllers/reviews_controller.rb
+++ flix/app/controllers/reviews_controller.rb
@@ -7,5 +7,24 @@ class ReviewsController < ApplicationController
 
   end
 
+  def new
+    @movie = Movie.find(params[:movie_id])
+    @review = @movie.reviews.new
+
+  end
+
+  def create
+
+    @movie = Movie.find(params[:movie_id])
+    #@review = @movie.reviews.new(params[:review])  FORBIDDEN ATTRIBUTES ERROR
+    @review = @movie.reviews.new(params.require(:review).permit(:name, :stars, :comment))
+
+    if @review.save
+      redirect_to movie_reviews_path(@movie), notice: "Thanks for your review"
+    else
+      render :new
+    end
+  end
+
 
 end
