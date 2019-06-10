cf1c004c0ceff0788ff2501343eb0c29fc966ee2
Nikita Kazakov <contactnikitakazakov@gmail.com>
2019-06-10 19:54:44 -0400

    Add RSPEC Testing

--- flix/spec/features/list_movies_spec.rb
+++ flix/spec/features/list_movies_spec.rb
@@ -3,8 +3,39 @@ require 'rails_helper'
 describe 'viewing list of movies' do
 
   it 'shows the movies' do
-    visit root_url
-    expect(page).to have_text("Movie Reviews")
+
+    movie1 = Movie.create(title: "Iron Man",
+                          description: "Tony Stark builds an armored suit to fight the throes of evil",
+                          rating: "PG-13",
+                          total_gross: 818412101.00,
+                          released_on: "2015-05-02",
+                          duration: 150)
+
+    movie2 = Movie.create(title: "Superman",
+                          rating: "PG",
+                          total_gross: 134218018.00,
+                          description: "Clark Kent grows up to be the greatest super-hero",
+                          released_on: "1978-12-15",
+                          duration: 120)
+
+    movie3 = Movie.create(title: "Spider-Man",
+                          rating: "PG-13",
+                          total_gross: 403706375.00,
+                          description: "Peter Parker gets bit by a genetically modified spider",
+                          released_on: "2002-05-03",
+                          duration: 110)
+
+    visit movies_url
+    expect(page).to have_text(movie1.title)
+    expect(page).to have_text(movie2.title)
+    expect(page).to have_text(movie3.title)
+    expect(page).to have_content("Iron Man")
+
+    expect(page).to have_text(movie1.rating)
+    #At least the first 10 characters of description
+    expect(page).to have_text(movie1.description[0..10])
+    expect(page).to have_text(movie1.released_on)
+
   end
 
 end
---
+++ flix/spec/features/show_movie_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe 'Viewing individual movies' do
+
+  it 'shows movies attributes' do
+    movie1 = Movie.create(title: "Iron Man",
+                          description: "Tony Stark builds an armored suit to fight the throes of evil",
+                          rating: "PG-13",
+                          total_gross: 818412101.00,
+                          released_on: "2015-05-02",
+                          duration: 150)
+
+    visit movie_url(movie1)
+    expect(page).to have_content(movie1.description)
+    expect(page).to have_text(movie1.released_on)
+    expect(page).to have_text(movie1.rating)
+    expect(page).to have_text(movie1.director)
+    expect(page).to have_text(movie1.duration)
+    #expect(page).to have_text(movie1.total_gross)
+    #add average ratings
+
+  end
+
+end
