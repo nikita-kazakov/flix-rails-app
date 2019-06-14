100533f8d0ba9c4eab0e8be3d0728a7c954401fe
Nikita Kazakov <contactnikitakazakov@gmail.com>
2019-06-14 13:43:52 -0400

    add future releases.

--- flix/app/controllers/movies_controller.rb
+++ flix/app/controllers/movies_controller.rb
@@ -1,9 +1,13 @@
 class MoviesController < ApplicationController
 
   def index
-    #@movies = ["Iron Man", "Superman", "Spider-Man", "Die Hard"] Hardcoded...
-    @movies = Movie.all
-    #@movies = Movie.released #<==This is defined in the Movie Model
+    #@movies = ["Iron Man", "Superman", "Spider-Man", "Die Hard"] #Hardcoded...
+    #@movies = Movie.all
+    @movies = Movie.released #<==This is defined in the Movie Model
+  end
+
+  def future_releases
+    @movies = Movie.future_releases
   end
 
   def show
@@ -13,8 +17,6 @@ class MoviesController < ApplicationController
   def edit
     @movie = Movie.find(params[:id])
   end
-
-
 
   def new
     @movie = Movie.new
