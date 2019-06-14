ff093c0ca0cb1364adb366e8e82da5819acb0ce4
Nikita Kazakov <contactnikitakazakov@gmail.com>
2019-06-13 21:06:04 -0400

    Add test specs for creating and deleting a page.

--- flix/spec/features/edit_movie_spec.rb
+++ flix/spec/features/edit_movie_spec.rb
@@ -11,4 +11,13 @@ describe 'Editing a movie' do
     expect(find_field('Title').value).to eq(movie.title)
   end
 
+  it 'updates the movie and shows the movies updated details' do
+    movie = Movie.create(movie_attributes)
+    visit movie_url(movie)
+    click_link 'Edit Movie'
+    expect(current_path).to eq(edit_movie_path(movie))
+
+    expect(find_field('Title').value).to eq(movie.title)
+  end
+
 end
